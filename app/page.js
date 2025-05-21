8a0904bdd4132dbe2dc8d7fae74ee5fac5b621d5
Viraj <viraj.tamhankar@gmail.com>
2025-05-21 15:11:20 +0530

    Updated button name

--- app/page.js
+++ app/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Isn't this exciting?</button>
+      <button className="btn">
+        Isn't this exciting? You've also added Git now :o
+      </button>
     </main>
   );
 }
